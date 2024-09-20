9d47c4d002989b72067ddc3e92ce518b7119c286
tam-vir-150 <tamvirshahabuddin@gmail.com>
2024-09-20 19:03:33 +0600

    products update

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -12,25 +12,25 @@ export default function Home() {
     <nav className="w-screen p-4 flex justify-around items-center">
       <Image className="ml-0" src={"/logo.png"} width={120} height={120} />
       <ul className="flex gap-10 text-gray-600 mr-10 uppercase font-bold">
-        <li className="hover:text-black hover:underline hover:underline-offset-4 duration-200">
+        <li className="hover:text-black hover:underline hover:underline-offset-4 duration-50">
           <a href="#" >Home</a>
         </li>
-        <li className="hover:text-black hover:underline hover:underline-offset-4 duration-200">
+        <li className="hover:text-black hover:underline hover:underline-offset-4 duration-50">
         <Dropdown title={"About Us"} items={about}/>
         </li>
-        <li className="hover:text-black hover:underline hover:underline-offset-4 duration-200">
+        <li className="hover:text-black hover:underline hover:underline-offset-4 duration-50">
           <Dropdown title={"Business Unit"} items={units}/>
         </li>
-        <li className="hover:text-black hover:underline hover:underline-offset-4 duration-200">
+        <li className="hover:text-black hover:underline hover:underline-offset-4 duration-50">
         <Dropdown title={"Products"} items={products}/>
         </li>
-        <li className="hover:text-black hover:underline hover:underline-offset-4 duration-200">
+        <li className="hover:text-black hover:underline hover:underline-offset-4 duration-50">
           <a href="#" >Clients</a>
         </li>
-        <li className="hover:text-black hover:underline hover:underline-offset-4 duration-200">
+        <li className="hover:text-black hover:underline hover:underline-offset-4 duration-50">
           <a href="#" >Gallery</a>
         </li>
-        <li className="hover:text-black hover:underline hover:underline-offset-4 duration-200">
+        <li className="hover:text-black hover:underline hover:underline-offset-4 duration-50">
           <a href="#" >Contact Us</a>
         </li>
         
--- src/components/products.js
+++ src/components/products.js
@@ -8,13 +8,13 @@ export default function Home() {
         <h1 className="text-gray-900 text-6xl font-black text-center">Products</h1>
         <hr className="w-1/2 hbar"/>
         <div className="w-full h-full bg-white grid grid-cols-3">
-      <div className="w-full h-full p-6 flex flex-col items-center justify-start">
+      <div className="w-full h-full p-6 flex flex-col items-center justify-center">
         <ProductCard title={"Double Jersey Machines"} description={descriptions.product1} bg={"/p1.jpg"}/>
       </div>
       <div className="w-full h-full p-6 flex flex-col items-center justify-center">
       <ProductCard title={"Electronic Jacquard Machines"} description={descriptions.product1} bg={"/p2.jpg"}/>
       </div>
-      <div className="w-full h-full p-6 flex flex-col items-center justify-end">
+      <div className="w-full h-full p-6 flex flex-col items-center justify-center">
       <ProductCard title={"Single Jersey Machines"} description={descriptions.product1} bg={"/p3.jpg"}/>
       </div>
     </div>
